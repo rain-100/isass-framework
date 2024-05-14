10437aa95dcf21d155eca3893524110018ba0f34
rain <lizhirong1@pcitech.com>
2024-05-14 14:52:49 +0800

    feat: ExceptionCatcher 捕捉的异常改为 Throwable

--- isass-core/src/main/java/vip/isass/framework/core/support/ExceptionCatcher.java
+++ isass-core/src/main/java/vip/isass/framework/core/support/ExceptionCatcher.java
@@ -193,7 +193,7 @@ public class ExceptionCatcher {
     public static <T> void consume(Consumer<T> consumer, T t) {
         try {
             consumer.accept(t);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             log.trace("ignore exception: ", e);
         }
     }
@@ -201,7 +201,7 @@ public class ExceptionCatcher {
     public static <T> void consume(T t, Consumer<T> consumer) {
         try {
             consumer.accept(t);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             log.trace("ignore exception: ", e);
         }
     }
@@ -209,7 +209,7 @@ public class ExceptionCatcher {
     public static <T, U> void biConsume(BiConsumer<T, U> consumer, T t, U u) {
         try {
             consumer.accept(t, u);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             log.trace("ignore exception: ", e);
         }
     }
@@ -217,7 +217,7 @@ public class ExceptionCatcher {
     public static <T, R> R functionOrDefault(Function<T, R> function, T t, R defaultValue) {
         try {
             return function.apply(t);
-        } catch (Exception e) {
+        } catch (Throwable e) {
             log.trace("ignore exception: ", e);
         }
         return defaultValue;
@@ -234,7 +234,7 @@ public class ExceptionCatcher {
     public static <T> T supplierOrDefault(Supplier<T> supplier, T defaultValue) {
         try {
             return supplier.get();
-        } catch (Exception e) {
+        } catch (Throwable e) {
             log.trace("ignore exception: ", e);
         }
         return defaultValue;
@@ -243,7 +243,7 @@ public class ExceptionCatcher {
     public static <T> boolean isNull(Supplier<T> supplier) {
         try {
             return supplier.get() == null;
-        } catch (Exception e) {
+        } catch (Throwable e) {
             return true;
         }
     }
@@ -259,7 +259,7 @@ public class ExceptionCatcher {
     public static boolean isBlank(Supplier<String> supplier) {
         try {
             return StrUtil.isBlank(supplier.get());
-        } catch (Exception e) {
+        } catch (Throwable e) {
             return true;
         }
     }
