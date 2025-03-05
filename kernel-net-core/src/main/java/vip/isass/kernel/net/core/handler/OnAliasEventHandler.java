68bbe4ce6dfcd6f83372d583d069c116f23489d9
rain <lizhirong1@pcitech.com>
2025-03-05 10:59:16 +0800

    feat: net session 新增设置 alias 命令

--- kernel-net-core/src/main/java/vip/isass/kernel/net/core/handler/OnAliasEventHandler.java
+++ kernel-net-core/src/main/java/vip/isass/kernel/net/core/handler/OnAliasEventHandler.java
@@ -167,58 +167,40 @@
  *
  */
 
-package vip.isass.kernel.net.core.message;
+package vip.isass.kernel.net.core.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import vip.isass.core.web.Resp;
+import vip.isass.kernel.net.core.message.Message;
+import vip.isass.kernel.net.core.message.MessageCmd;
+import vip.isass.kernel.net.core.server.Server;
+import vip.isass.kernel.net.core.session.ISessionService;
 
 /**
- * 内置消息路由
+ * 登录事件处理器
  *
- * @author Rain
+ * @author rain
  */
-public interface MessageCmd {
+@Order(-1)
+@Configuration
+@ConditionalOnBean(Server.class)
+public class OnAliasEventHandler implements OnMessageEventHandler<String> {
 
-    /**
-     * core 的 cmd 前置
-     */
-    String CORE_PREFIX = "/core/";
+    @Autowired
+    private ISessionService sessionService;
 
-    /**
-     * PING
-     */
-    String PING = "/core/ping";
+    @Override
+    public String getCmd() {
+        return MessageCmd.ALIAS;
+    }
 
-    /**
-     * PONG
-     */
-    String PONG = "/core/pong";
-
-    /**
-     * 登录请求
-     */
-    String LOGIN = "/core/login";
-
-    /**
-     * 登出请求
-     */
-    String LOGOUT = "/core/logout";
-
-    /**
-     * 设置别名
-     */
-    String ALIAS = "/core/alias";
-
-    /**
-     * 异常报错
-     */
-    String ERROR = "/core/exception";
-
-    /**
-     * 客户端发起广播消息
-     */
-    String CLIENT_SEND_BROADCAST = "/core/clientSendBroadcast";
-
-    /**
-     * 客户端发起p2p消息
-     */
-    String CLIENT_P2P = "/core/client/p2p";
+    @Override
+    public Object onMessage(Message message, String alias) {
+        sessionService.setAlias(message.getSenderSessionId(), alias);
+        return Resp.bizSuccess("set alias success");
+    }
 
 }
